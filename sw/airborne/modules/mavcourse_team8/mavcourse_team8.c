37f1937cb7af5dadfa527f29ddfe8cb20bf36b86
mbrummelhuis <mbrummelhuis@gmail.com>
2021-03-10 16:33:45 +0100

    Nieuwe module 'mavcourse_team8'
    Nieuwe .c, .h en XML files aangemaakt onder de bovenstaande naam. Hierin zullen we onze firmware schrijven. Kijk vooral ook naar andere voorbeelden (orange avoider bv) als je dingen wil toevoegen.

---
+++ sw/airborne/modules/mavcourse_team8/mavcourse_team8.c
@@ -0,0 +1,28 @@
+/*
+ * Copyright (C) Martijn Brummelhuis <m.b.j.brummelhuis@student.tudelft.nl>
+ *
+ * This file is part of paparazzi
+ *
+ * paparazzi is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2, or (at your option)
+ * any later version.
+ *
+ * paparazzi is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with paparazzi; see the file COPYING.  If not, see
+ * <http://www.gnu.org/licenses/>.
+ */
+
+/** @file "modules/mavcourse_team8/mavcourse_team8.c"
+ * @author Martijn Brummelhuis <m.b.j.brummelhuis@student.tudelft.nl>
+ * Team 8 mav course module.
+ */
+
+#include "modules/mavcourse_team8/mavcourse_team8.h"
+
+
---
+++ sw/airborne/modules/mavcourse_team8/mavcourse_team8.h
@@ -0,0 +1,30 @@
+/*
+ * Copyright (C) Martijn Brummelhuis <m.b.j.brummelhuis@student.tudelft.nl>
+ *
+ * This file is part of paparazzi
+ *
+ * paparazzi is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2, or (at your option)
+ * any later version.
+ *
+ * paparazzi is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with paparazzi; see the file COPYING.  If not, see
+ * <http://www.gnu.org/licenses/>.
+ */
+
+/** @file "modules/mavcourse_team8/mavcourse_team8.h"
+ * @author Martijn Brummelhuis <m.b.j.brummelhuis@student.tudelft.nl>
+ * Team 8 mav course module.
+ */
+
+#ifndef MAVCOURSE_TEAM8_H
+#define MAVCOURSE_TEAM8_H
+
+
+#endif  // MAVCOURSE_TEAM8_H
