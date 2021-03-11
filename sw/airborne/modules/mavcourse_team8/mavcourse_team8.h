40aec1c84fa909f86c7d5d90b987ecb03c750834
mbrummelhuis <mbrummelhuis@gmail.com>
2021-03-11 19:36:16 +0100

    Module zo opgezet dat je hem nu zou moeten kunnen zien in paparazzi (lukt bij mij). Alle code en shite gekopieerd van de orange_avoid_guided

--- sw/airborne/modules/mavcourse_team8/mavcourse_team8.h
+++ sw/airborne/modules/mavcourse_team8/mavcourse_team8.h
@@ -1,30 +1,41 @@
 /*
- * Copyright (C) Martijn Brummelhuis <m.b.j.brummelhuis@student.tudelft.nl>
+ * Copyright (C) Kirk Scheper <kirkscheper@gmail.com>
  *
  * This file is part of paparazzi
  *
- * paparazzi is free software; you can redistribute it and/or modify
- * it under the terms of the GNU General Public License as published by
- * the Free Software Foundation; either version 2, or (at your option)
- * any later version.
+ */
+/**
+ * @file "modules/orange_avoider/orange_avoider_guided.c"
+ * @author Kirk Scheper
+ * This module is an example module for the course AE4317 Autonomous Flight of Micro Air Vehicles at the TU Delft.
+ * This module is used in combination with a color filter (cv_detect_color_object) and the guided mode of the autopilot.
+ * The avoidance strategy is to simply count the total number of orange pixels. When above a certain percentage threshold,
+ * (given by color_count_frac) we assume that there is an obstacle and we turn.
  *
- * paparazzi is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU General Public License for more details.
+ * The color filter settings are set using the cv_detect_color_object. This module can run multiple filters simultaneously
+ * so you have to define which filter to use with the ORANGE_AVOIDER_VISUAL_DETECTION_ID setting.
+ * This module differs from the simpler orange_avoider.xml in that this is flown in guided mode. This flight mode is
+ * less dependent on a global positioning estimate as witht the navigation mode. This module can be used with a simple
+ * speed estimate rather than a global position.
  *
- * You should have received a copy of the GNU General Public License
- * along with paparazzi; see the file COPYING.  If not, see
- * <http://www.gnu.org/licenses/>.
+ * Here we also need to use our onboard sensors to stay inside of the cyberzoo and not collide with the nets. For this
+ * we employ a simple color detector, similar to the orange poles but for green to detect the floor. When the total amount
+ * of green drops below a given threshold (given by floor_count_frac) we assume we are near the edge of the zoo and turn
+ * around. The color detection is done by the cv_detect_color_object module, use the FLOOR_VISUAL_DETECTION_ID setting to
+ * define which filter to use.
  */
 
-/** @file "modules/mavcourse_team8/mavcourse_team8.h"
- * @author Martijn Brummelhuis <m.b.j.brummelhuis@student.tudelft.nl>
- * Team 8 mav course module.
- */
+#ifndef ORANGE_AVOIDER_GUIDED_H
+#define ORANGE_AVOIDER_GUIDED_H
 
-#ifndef MAVCOURSE_TEAM8_H
-#define MAVCOURSE_TEAM8_H
+// settings
+extern float oag_color_count_frac;  // obstacle detection threshold as a fraction of total of image
+extern float oag_floor_count_frac;  // floor detection threshold as a fraction of total of image
+extern float oag_max_speed;         // max flight speed [m/s]
+extern float oag_heading_rate;      // heading rate setpoint [rad/s]
 
+extern void orange_avoider_guided_init(void);
+extern void orange_avoider_guided_periodic(void);
 
-#endif  // MAVCOURSE_TEAM8_H
+#endif
+
